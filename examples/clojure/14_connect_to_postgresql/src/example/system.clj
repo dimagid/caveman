842bc78fecd8fb0adb17e6109ac0b8862c8b0a29
Ethan McCue <ethan@mccue.dev>
2024-10-22 17:48:05 -0400

    Connect to postgres

---
+++ examples/clojure/14_connect_to_postgresql/src/example/system.clj
@@ -0,0 +1,41 @@
+(ns example.system
+  (:require [example.routes :as routes]
+            [next.jdbc.connection :as connection]
+            [ring.adapter.jetty :as jetty])
+  (:import (com.zaxxer.hikari HikariDataSource)
+           (org.eclipse.jetty.server Server)))
+
+(set! *warn-on-reflection* true)
+
+(defn start-db
+  []
+  (connection/->pool HikariDataSource
+                     {:dbtype "postgres"
+                      :dbname "postgres"
+                      :username "postgres"
+                      :password "postgres"}))
+
+(defn stop-db
+  [db]
+  (HikariDataSource/.close db))
+
+(defn start-server
+  [system]
+  (jetty/run-jetty
+   (partial #'routes/root-handler system)
+   {:port 9999
+    :join? false}))
+
+(defn stop-server
+  [server]
+  (Server/.stop server))
+
+(defn start-system
+  []
+  (let [system-so-far {::db (start-db)}]
+    (merge system-so-far {::server (start-server system-so-far)})))
+
+(defn stop-system
+  [system]
+  (stop-db (::db system))
+  (stop-server (::server system)))
