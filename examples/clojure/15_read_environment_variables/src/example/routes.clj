a35bd66fa2c1fb466cc07be4dc5d4bf1ebcc5bda
Ethan McCue <ethan@mccue.dev>
2024-10-22 18:22:46 -0400

    dotenv

---
+++ examples/clojure/15_read_environment_variables/src/example/routes.clj
@@ -0,0 +1,54 @@
+(ns example.routes
+  (:require [clojure.tools.logging :as log]
+            [example.system :as-alias system]
+            [hiccup2.core :as hiccup]
+            [next.jdbc :as jdbc]
+            [reitit.ring :as reitit-ring]))
+
+(defn hello-handler
+  [{::system/keys [db]} _request]
+  (let [{:keys [planet]} (jdbc/execute-one!
+                          db
+                          ["SELECT 'earth' as planet"])]
+    {:status 200
+     :headers {"Content-Type" "text/html"}
+     :body (str
+            (hiccup/html
+             [:html
+              [:body
+               [:h1 (str "Hello, " planet)]]]))}))
+
+(defn goodbye-handler
+  [_system _request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body (str
+          (hiccup/html
+           [:html
+            [:body
+             [:h1 "Goodbye, world"]]]))})
+
+(defn routes
+  [system]
+  [["/"        {:get {:handler (partial #'hello-handler system)}}]
+   ["/goodbye" {:get {:handler (partial #'goodbye-handler system)}}]])
+
+(defn not-found-handler
+  [_request]
+  {:status 404
+   :headers {"Content-Type" "text/html"}
+   :body (str
+          (hiccup/html
+           [:html
+            [:body
+             [:h1 "Not Found"]]]))})
+
+(defn root-handler
+  [system request]
+  (log/info (str (:request-method request) " - " (:uri request)))
+  (let [handler (reitit-ring/ring-handler
+                 (reitit-ring/router
+                  (routes system))
+                 #'not-found-handler)]
+    (handler request)))
+
