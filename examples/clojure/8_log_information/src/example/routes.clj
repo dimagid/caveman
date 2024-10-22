bdcc8c581b36407469b6e6f140773dc3c0504c50
Ethan McCue <ethan@mccue.dev>
2024-10-22 13:21:56 -0400

    checkpoint

---
+++ examples/clojure/8_log_information/src/example/routes.clj
@@ -0,0 +1,36 @@
+(ns example.routes
+  (:require [reitit.ring :as reitit-ring]
+            [clojure.tools.logging :as log]))
+
+(defn hello-handler
+  [system request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Hello, world"})
+
+(defn goodbye-handler
+  [system request]
+  {:status 200
+   :headers {"Content-Type" "text/html"}
+   :body "Goodbye, world"})
+
+(defn routes
+  [system]
+  [["/"        {:get {:handler (partial #'hello-handler system)}}]
+   ["/goodbye" {:get {:handler (partial #'goodbye-handler system)}}]])
+
+(defn not-found-handler
+  [_request]
+  {:status 404
+   :headers {"Content-Type" "text/html"}
+   :body "Not Found"})
+
+(defn root-handler
+  [system request]
+  (log/info (str (:request-method request) " - " (:uri request)))
+  (let [handler (reitit-ring/ring-handler
+                  (reitit-ring/router
+                    (routes system))
+                  #'not-found-handler)]
+    (handler request)))
+
