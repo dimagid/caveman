f0225f1ddae3b810d925f8d0746c5516d99c8bd1
Ethan McCue <ethan@mccue.dev>
2024-10-23 11:47:56 -0400

    Dynamic sql

---
+++ examples/clojure/24_dynamically_generate_sql/src/example/cave/jobs.clj
@@ -0,0 +1,18 @@
+(ns example.cave.jobs
+  (:require [example.system :as-alias system]
+            [honey.sql :as sql]
+            [next.jdbc :as jdbc])
+  (:import (java.util UUID)))
+
+(defn process-cave-insert
+  [{::system/keys [db]} _job-type payload]
+  (jdbc/execute!
+   db
+   (sql/format
+    {:insert-into :prehistoric/hominid
+     :values [{:name    "Grunk"
+               :cave_id (UUID/fromString (:id payload))}]})))
+
+(defn handlers
+  []
+  {:prehistoric.cave/insert #'process-cave-insert})
